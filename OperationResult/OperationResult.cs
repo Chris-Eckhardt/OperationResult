2e4710a262910a0b7a1913c9b92f2c9150181c70
Chris Eckhardt <ceckhardt06@gmail.com>
2024-07-10 10:07:24 -0400

    added code doc summaries for ResultObject methods

--- OperationResult/OperationResult.cs
+++ OperationResult/OperationResult.cs
@@ -6,21 +6,38 @@ public class ResultObject
 
     private static ErrorTag ErrorTag { get; }
 
+    /// <summary>
+    /// An object representing an operation that completed successfully.
+    /// </summary>
+    /// <returns></returns>
     public static SuccessTag Ok()
     {
         return SuccessTag;
     }
 
+    /// <summary>
+    /// An object representing an operation that completed successfully with a return value.
+    /// </summary>
     public static SuccessTag<TResult> Ok<TResult>(TResult result)
     {
         return new SuccessTag<TResult>(result);
     }
 
+    /// <summary>
+    /// An object representing an operation that completed unsuccessfully.
+    /// </summary>
+    /// <returns></returns>
     public static ErrorTag Error()
     {
         return ErrorTag;
     }
 
+    /// <summary>
+    /// An object representing an operation that completed unuccessfully with an error.
+    /// </summary>
+    /// <typeparam name="TError"></typeparam>
+    /// <param name="error"></param>
+    /// <returns></returns>
     public static ErrorTag<TError> Error<TError>(TError error)
     {
         return new ErrorTag<TError>(error);
